ca73a873aa41c0035d9b13764a4ac745e14a42da
Anthony <74481822+anthonykid@users.noreply.github.com>
2021-06-14 01:50:37 +0800

    listview seperated
    its diffrent with the listview builder too, this listview seperated are going to show the list view that devided with a seperator, the use of seperated is close enough with builder, the diffrencies is that seperated got 1 parameter that required seperator builder that return the widget who is gonna have a role as seperator

--- main.dart
+++ main.dart
@@ -13,41 +13,38 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ScrollingScreen(), 
+      home: ScrollingScreen(),
     );
   }
 }
 
 class ScrollingScreen extends StatelessWidget {
-  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
+      body:     ListView.separated(
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 250,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            border: Border.all(color: Colors.black),
+          ),
+          child: Center(
+            child: Text(
+              '${numberList[index]}',
+              style: TextStyle(fontSize: 50),
             ),
-            child: Center(
-              child: Text(
-                '${numberList[index]}',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          );
-        },
-        itemCount: numberList.length,
-      ),
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider();
+      },
+      itemCount: numberList.length,
+    ),
     );
   }
 }
-
-// Menggunakan ListView.builder
-
-// Selain mengisi parameter children dari ListView seperti sebelumnya, kita juga bisa memanfaatkan method ListView.builder. ListView.builder lebih cocok digunakan pada ListView dengan jumlah item yang cukup besar. Ini karena Flutter hanya akan merender tampilan item yang terlihat di layar dan tidak me-render seluruh item ListView di awal.
-
-// ListView.builder memerlukan dua parameter yaitu itemBuilder dan itemCount. Parameter itemBuilder merupakan fungsi yang mengembalikan widget untuk ditampilkan. Sedangkan itemCount kita isi dengan jumlah seluruh item yang ingin ditampilkan.
